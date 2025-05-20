de1c97eb1d6e3eb7319f39179e62219fbba0a2d9
Rudra savani <ritasavani6446@gmail.com>
2025-05-20 12:04:45 +0530

    Add files via upload
    Signed-off-by: Rudra savani <ritasavani6446@gmail.com>

---
+++ exercise_17.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int count,number,sum;
+
+    printf("Enter your number. :- ");
+    scanf("%d",&number);
+
+    count = 1;
+
+    
+    if (number % count == 0) 
+    {
+        sum = sum + count;
+    }
+}
